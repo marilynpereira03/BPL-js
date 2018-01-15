feab2d736a8aac8782cf87f3275e05acd964d617
marilynpereira03 <marilyn.pereira@harbingergroup.com>
2018-01-15 15:04:31 +0530

    Changed javascript to depict class structure.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-bpl = {
+/*bpl = {
 	crypto : require("./lib/transactions/crypto.js"),
 	delegate : require("./lib/transactions/delegate.js"),
 	signature : require("./lib/transactions/signature.js"),
@@ -18,4 +18,57 @@ for (var method in libCrypto) {
 	bpl.crypto[method] = libCrypto[method]
 }
 
-module.exports = bpl;
+module.exports = bpl;*/
+
+let Crypto = require("./lib/transactions/crypto.js");
+let Delegate = require("./lib/transactions/delegate.js");
+let Signature = require("./lib/transactions/signature.js");
+let Transaction = require("./lib/transactions/transaction.js");
+let Vote = require("./lib/transactions/vote.js");
+let Ipfs = require("./lib/transactions/ipfs.js");
+let Networks = require("./lib/networks.js");
+let Slots = require("./lib/time/slots.js");
+let ECPair = require("./lib/ecpair.js");
+let HDNode = require("./lib/hdnode.js");
+let ECSignature = require("./lib/ecsignature.js");
+
+class Bpl {
+  constructor(config) {
+		//default config for wbx
+    let defaultConfig = {
+      interval: 15,
+      delegates: 4,
+			networkVersion: 73
+    };
+
+    let finalConfig = {
+      interval: config.interval || defaultConfig.interval,
+      delegates: config.delegates || defaultConfig.delegates,
+			networkVersion: config.networkVersion || defaultConfig.networkVersion
+    };
+
+		this.crypto = new Crypto();
+		this.delegate = new Delegate();
+		this.signature = new Signature();
+		this.transaction = new Transaction();
+		this.vote = new Vote();
+		this.ipfs = new Ipfs();
+		this.networks = new Networks();
+    this.slots = new Slots();
+		this.ECPair = new ECPair();
+		this.HDNode = new HDNode();
+		this.ECSignature = new ECSignature();
+
+    this.slots.setConfig(finalConfig);
+		this.crypto.setConfig(finalConfig);
+  }
+
+//TODO
+	// extra aliases for bitcoinlib-js compatibility
+	// var libCrypto = require('./lib/crypto.js')
+	// for (var method in libCrypto) {
+	// 	bpl.crypto[method] = libCrypto[method]
+	// }
+}
+
+module.exports = Bpl;
