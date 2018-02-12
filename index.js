3ce70e1d65744f23530e8b9e21321133254c2e71
Avinash <avinash.vazratkar@harbingergroup.com>
2018-02-12 18:35:27 +0530

    Update for Smart contract transaction.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const delegate = require('./lib/transactions/delegate.js');
 const signature = require('./lib/transactions/signature.js');
 const transaction = require('./lib/transactions/transaction.js');
 const vote = require('./lib/transactions/vote.js');
+const smartContract = require('./lib/transactions/smartContract.js');
 const ipfs = require('./lib/transactions/ipfs.js');
 const networks = require('./lib/networks.js');
 const slots = require('./lib/time/slots.js');
@@ -27,6 +28,7 @@ class BplClass {
 		this.delegate = new delegate.DelegateClass(finalConfig);
 		this.signature = new signature.SignatureClass(finalConfig);
 		this.transaction = new transaction.TransactionClass(finalConfig);
+		this.smartContract =  new smartContract.SmartContractClass(finalConfig);
 		this.vote = new vote.VoteClass(finalConfig);
 		this.ipfs = new ipfs.IpfsClass(finalConfig);
 		this.networks = networks;
